fd25caca015d37e11330c932be9019276e7b46e6
ooluchi <ooluchiteeto@gmail.com>
2024-08-23 00:01:28 +0100

    changes

--- scissor/src/components/Header.tsx
+++ scissor/src/components/Header.tsx
@@ -1,11 +1,14 @@
+import React from 'react';
+
+
 const Header: React.FC = () => (
-    <header className="flex justify-between items-center p-4">
-      <div className="text-3xl font-bold text-purple-500">Linkly</div>
-      <div>
-        <button className="btn">Login</button>
-        <button className="btn ml-4">Register Now</button>
-      </div>
-    </header>
-  );
+  <header className="container">
+    <div className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Scissor</div>
+    <div>
+      <button className="btn">Login</button>
+      <button className="btn ml-4">Register Now</button>
+    </div>
+  </header>
+);
 
 export default Header;
--- scissor/src/components/UrlList.tsx
+++ scissor/src/components/UrlList.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const UrlList: React.FC = () => {
     // Sample data
     const urls = [
